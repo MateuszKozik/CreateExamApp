62fd44fcf91abc657b67ba078986e4a165a2e089
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-01-12 16:36:08 +0100

    Create simple react router structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/login" component={Login} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
