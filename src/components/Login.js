cba44b14be01af6ba8fff210fe0d39e74d3b0623
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-01-12 16:45:52 +0100

    Create login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
-
+import { TextField, Grid, Typography, Button } from "@material-ui/core";
 class Login extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<Grid container style={{ textAlign: "center", marginTop: 10 }}>
+				<Grid item xs={12}>
+					<Typography variant="h3">Logowanie</Typography>
+				</Grid>
+
+				<Grid item xs={12} style={{ marginTop: 20 }}>
+					<TextField
+						placeholder="Wpisz email"
+						id="email"
+						name="email"
+						type="email"
+						label="Email"
+						variant="outlined"
+					/>
+				</Grid>
+
+				<Grid item xs={12} style={{ marginTop: 20 }}>
+					<TextField
+						type="password"
+						id="password"
+						name="password"
+						label="Hasło"
+						placeholder="Wpisz hasło"
+						variant="outlined"
+					/>
+				</Grid>
+				<Grid item xs={12} style={{ marginTop: 20 }}>
+					<Button variant="contained" color="primary">
+						Zaloguj
+					</Button>
+				</Grid>
+			</Grid>
+		);
 	}
 }
 
