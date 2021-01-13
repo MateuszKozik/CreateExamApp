736365410d6db9c221e5734abd007e014c1cb51d
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-01-13 18:43:09 +0100

    Create table with courses list

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,7 +1,20 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import { getCourses } from "../actions/courseActions";
+import {
+	IconButton,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Tooltip
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+
+import DeleteIcon from "@material-ui/icons/Delete";
 
 class Courses extends Component {
 	state = {
@@ -10,18 +23,92 @@ class Courses extends Component {
 
 	componentDidMount() {
 		getCourses().then((res) => {
-			console.log(res.data.records);
 			this.setState({ courses: res.data.records });
 		});
 	}
 
 	render() {
+		const { courses } = this.state;
 		return (
 			<>
 				<Navbar />
-				<Grid container style={{ textAlign: "center", marginTop: 10 }}>
+				<Grid
+					container
+					spacing={1}
+					style={{ textAlign: "center", marginTop: 10 }}
+				>
 					<Grid item xs={12}>
 						<Typography variant="h5">Przedmioty</Typography>
+					</Grid>
+					<Grid item xs={12} style={{ textAlign: "left", marginLeft: 10 }}>
+						<Button variant="contained" color="primary">
+							Dodaj przedmiot
+						</Button>
+					</Grid>
+					<Grid item xs={12}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell>
+											<Typography>
+												<b>Nazwa przedmiotu</b>
+											</Typography>
+										</TableCell>
+										<TableCell>
+											<Typography align="center">
+												<b>Opis</b>
+											</Typography>
+										</TableCell>
+										<TableCell>
+											<Typography align="center">
+												<b>Akcje</b>
+											</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{courses
+										? courses.map((course, index) => {
+												return (
+													<TableRow key={index}>
+														<TableCell>
+															<Typography>{course.title}</Typography>
+														</TableCell>
+														<TableCell>
+															<Typography align="center">
+																{course.description}
+															</Typography>
+														</TableCell>
+														<TableCell align="center">
+															<Tooltip title="Edytuj">
+																<IconButton
+																	color="primary"
+																	onClick={() => {
+																		console.log("edycja");
+																	}}
+																>
+																	<EditIcon />
+																</IconButton>
+															</Tooltip>
+															<Tooltip title="Edytuj">
+																<IconButton
+																	color="secondary"
+																	onClick={() => {
+																		console.log("usuwanie");
+																	}}
+																>
+																	<DeleteIcon />
+																</IconButton>
+															</Tooltip>
+														</TableCell>
+													</TableRow>
+												);
+										  })
+										: null}
+								</TableBody>
+							</Table>
+						</TableContainer>
 					</Grid>
 				</Grid>
 			</>
